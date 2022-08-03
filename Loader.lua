9eadcbc24bd16e206bcc0077180500914319a423
Josh <92058789+DevXternal@users.noreply.github.com>
2022-08-02 17:12:05 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,21 +4,11 @@
     Created with love to you by DevXternal
 ]]--
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Nexus Loader", "Synapse")
-
---Loader
-local Home = Window:NewTab("Load")
-local HomeSection = Home:NewSection("Choose a version:")
-
-HomeSection:NewButton("Nexus Hub Normal", "Load regular Nexus Hub", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DevXternal/Nexus/main/Nexus.lua"))()
-end)
-
-HomeSection:NewButton("Nexus Calico", "Load server-side script executor.", function() 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DevXternal/Nexus/main/Nexus%20Calico.lua"))()
-end)
-
-HomeSection:NewButton("Nexus Trico", "Coming soon", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DevXternal/Nexus/main/Nexus%20Trico.lua"))()
-end)
+game.StarterGui:SetCore("SendNotification", {Title="Whitelisting..."; Text="pls wait lol."; Duration=5;})
+wait(2)
+if game.Players.LocalPlayer.Name == "cambox13" then
+    game.StarterGui:SetCore("SendNotification", {Title="Welcome!"; Text="Hello DevXternal, loading diff version."; Duration=5;})
+else
+    game.StarterGui:SetCore("SendNotification", {Title="Whitelisting..."; Text="Hello" ..game.Players.LocalPlayer.Name; Duration=5;})
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DevXternal/Nexus/main/Nexus.lua"), true)()
+end
